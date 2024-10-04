ae0f50608bf951e0ad5ccc8b319675bc378352be
Mirella Barros <mirella@mirellabarros.com>
2024-10-03 22:01:52 -0300

    Primeira versão do programa

---
+++ main.py
@@ -0,0 +1,17 @@
+# Função recebe o salário fixo e o valor em vendas 
+# Retorna o valor da comissão e o pagamento total
+def calcula_pagamento(salario, vendas):
+  comissao = vendas * 0.15
+  pagamento = salario + comissao
+  return comissao, pagamento
+
+# Obtém o nome do vendedor, salário fixo e o valor em vendas
+nome = input("Entre com o nome do vendedor: ")
+salario = float(input("Informe o salário: "))
+vendas = float(input("Informe o valor em vendas: "))
+
+# Chama a função calcula_pagamento para obter o valor da comissão e o pagamento total
+comissao, pagamento = calcula_pagamento(salario, vendas)
+
+# Escreve o resultado na tela
+print("{0} obteve {1:.2f} de comissao e vai receber {2:.2f}".format(nome, comissao, pagamento))
