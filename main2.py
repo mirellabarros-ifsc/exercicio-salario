02e874af22a10c1dee310d043847658865e3c91d
Mirella Barros <mirella.b@aluno.ifsc.edu.br>
2024-10-07 12:41:53 -0300

    versão calcula salário

--- main2.py
+++ main2.py
@@ -1,3 +1,16 @@
-for i in range(1,11):
-    print("o número é", i)
-    
+#coding: utf-8
+
+def ComissaoSalario():  
+    Nome = input("Entre com o nome do vendedor: ")
+    SalarioFixo = float(input("Informe o salário: "))
+    Vendas = float(input("Informe o total de vendas: "))
+    Comissao = 0.15*Vendas 
+    PagamentoEsperado=SalarioFixo+Comissao
+    return Nome, Comissao, PagamentoEsperado
+
+
+
+if __name__=="__main__": 
+    Nome, Comissao, PagamentoEsperado= ComissaoSalario()
+    strg = "{0} obteve R$ {1:.2f} de comissão e vai receber {2:.2f}".format(Nome,Comissao, PagamentoEsperado)
+    print(strg) 
